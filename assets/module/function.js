135463399cacad8ec439e155a6d403ea6e37e84c
AlejoDeOli <alejoideoli@hotmail.com>
2024-02-26 21:48:28 -0300

    Add: Details and Filters

--- assets/module/function.js
+++ assets/module/function.js
@@ -1,4 +1,4 @@
-export {crearCard}
+export {printCards, filtrerForTitle, printOptions, filterForGenre}
 
 
 
@@ -6,10 +6,45 @@ export {crearCard}
 function crearCard(movie){
     return `
     <article class="flex flex-wrap w-80 rounded p-5 mt-4 bg-slate-700 shadow-[0_5px_20px_rgba(75,85,99)]">
-        <img class="h-44 rounded-md" src="${movie.image}" alt="Imagen de una ${movie.nombre}">
+        <img class="h-44 rounded-md" src="${movie.image}" alt="Image ${movie.title}">
         <h2 class="font-bold text-purple-300 text-xl w-full">${movie.title}</h2>
         <h3 class="italic underline text-xl font-small text-slate-300">${movie.tagline}</h3>
         <p class="text-lg text-white line-clamp-5 overflow-auto">${movie.overview}</p>
+        <a class="text-md underline text-purple-500" href="./detail.html?id=${movie.id}&name=${movie.title}">Details</a>
     </article>
     `;
+}
+
+
+
+function printCards(listCards, elemento){
+    let template = " "
+    for (const movieIterada of listCards) {
+        template += crearCard(movieIterada)
+    }
+    elemento.innerHTML = template
+}
+
+
+
+function filtrerForTitle(listMovie, title){
+    return listMovie.filter( movie => movie.title.toLowerCase().startsWith(title.toLowerCase()) )
+    
+}
+
+
+
+const printOptions = function (array, elemento){
+    let options = ""
+    const genresRepeated = array.flatMap((movie) => movie.genres).filter((value, index, arreglo) => arreglo.indexOf(value) === index);
+    genresRepeated.forEach((genre) => {
+        options += `<option value="${genre}">${genre}</option>`
+    });
+    elemento.innerHTML = `<option value="all">All Genres</option>` + options;
+};
+
+
+
+const filterForGenre = function (array, genre){
+    return array.filter((movie) => movie.genres.includes(genre))
 }
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import {crearCard} from "./assets/module/function.js"
-
-
+import {printCards, filtrerForTitle, printOptions, filterForGenre} from "./assets/module/function.js"
 
 
 let divMain = document.createElement (`div`);
@@ -8,10 +6,34 @@ divMain.id = `contenedor`
 divMain.className = "flex flex-row justify-evenly flex-wrap gap-5 mt-8 mb-24"
 document.querySelector('main').appendChild(divMain);
 
+const $inputText = document.getElementById(`inputText`)
+const select = document.getElementById(`select`)
+const genres = movies.map(movie => movie.genres).flat()
+const setgenres = new Set( genres )
 
 
 
 
-for (const movieIterada of movies) {
-    divMain.innerHTML += crearCard(movieIterada)
-}
+
+
+
+
+select.addEventListener("change", () => {
+    const movieFilters = filterForGenre(movies, select.value)
+    if(select.value === "all"){
+        printCards(movies, divMain)
+    }else{
+        printCards(movieFilters, divMain)
+    }
+})
+
+
+$inputText.addEventListener(`input`, () => {
+    const titleFilters = filtrerForTitle(movies, $inputText.value)
+    printCards(titleFilters, divMain)
+})
+
+
+
+printOptions(movies, select)
+printCards(movies, divMain)
